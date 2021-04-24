afdabaf27d37edf098bae45c21d2ad2add8297be
Shrewkin <strizhevp@gmail.com>
2021-04-24 04:35:34 -0700

    Moved to Unity
    Moved the entire game project to Unity

--- UnderwaterGame/Intermediate/Build/Win64/UnderwaterGameEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.28.29337
-#include "C:/Users/Striz/Desktop/UnderwaterGame/UnderwaterGame/Intermediate/Build/Win64/UnderwaterGameEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
