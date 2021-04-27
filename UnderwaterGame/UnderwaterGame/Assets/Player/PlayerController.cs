bd0331a0e56eaca145d545ab33a9eec577547e17
Peter S <strizhevp@gmail.com>
2021-04-26 17:54:48 -0700

    Camera Attachment

--- UnderwaterGame/UnderwaterGame/Assets/Player/PlayerController.cs
+++ UnderwaterGame/UnderwaterGame/Assets/Player/PlayerController.cs
@@ -17,17 +17,11 @@ public class PlayerController : MonoBehaviour
 
 	void Update()
 	{
-
 		// Use input up and down for direction, multiplied by speed
 		moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 		moveDirection = transform.TransformDirection(moveDirection);
 		moveDirection *= speed;
-
-		// Vector3 mouseLoc = Input.mousePosition;
-		// Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(new Vector3(mouseLoc.x, mouseLoc.y, controller.transform.position.y));
-		// Vector3 forwardVec = mouseWorld - controller.transform.position;
-		// controller.transform.rotation = Quaternion.LookRotation(forwardVec, Vector3.up);
-
+		
 		// Move Character Controller
 		controller.Move(moveDirection * Time.deltaTime);
 	}
